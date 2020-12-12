cc95bcda3c7bcaaaf84cd13c8b5f2ab13f8cd6c7
mikaela.pisani <mikaela.pisani@rootstrap.com>
2020-12-12 12:09:10 -0300

    add dummy test

--- dags/dummy_test.py
+++ dags/dummy_test.py
@@ -11,6 +11,16 @@ sys.path.insert(0, '/opt/airflow/dags/airflow.git/dags')
 #from utils.general import groups, defaults
 #from utils.s3 import helper as s3_helper
 
+dag_args = {
+    "owner": "airflow",
+    "depends_on_past": False,
+    "start_date": datetime(2020, 9, 7),
+    "email": ["mikaela.pisani@rootstrap.com"],
+    "email_on_failure": False,
+    "email_on_retry": False,
+    "retries": 1,
+    "retry_delay": timedelta(minutes=5)
+}
 
 def test():
 	print(os.environ.get('AIRFLOW__CORE__DAGS_FOLDER'))
@@ -19,7 +29,7 @@ def test():
 	    
 
 
-with DAG(dag_id="s3_normalize_csv", default_args=defaults.dag_args, schedule_interval= '@once') as dag:
+with DAG(dag_id="dummy_test", default_args=dag_args, schedule_interval= '@once') as dag:
 
     start = PythonOperator(task_id='task-start', python_callable=test, dag=dag)
 
