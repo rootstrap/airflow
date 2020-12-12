534a4fe485a9f221b832ebc206c4d5ebfad709de
mikaela.pisani <mikaela.pisani@rootstrap.com>
2020-12-12 15:06:03 -0300

    add env variable

--- dags/dummy_test.py
+++ dags/dummy_test.py
@@ -9,15 +9,13 @@ from datetime import datetime, timedelta
 from airflow.models import Variable
 
 import sys, os
-AIRFLOW__CORE__DAGS_FOLDER = Variable.get("AIRFLOW__CORE__DAGS_FOLDER")
-sys.path.insert(0,AIRFLOW__CORE__DAGS_FOLDER)
-sys.path.insert(0,AIRFLOW__CORE__DAGS_FOLDER + '/utils')
+sys.path.insert(0,os.environ('AIRFLOW__CORE__DAGS_FOLDER'))
 
-from importlib import import_module
-defaults = import_module('general.defaults')
-s3_helper = import_module('s3.helper')
-#from utils.general import groups, defaults
-#from utils.s3 import helper as s3_helper
+#from importlib import import_module
+#defaults = import_module('general.defaults')
+#s3_helper = import_module('s3.helper')
+from utils.general import groups, defaults
+from utils.s3 import helper as s3_helper
 
 dag_args = {
     "owner": "airflow",
