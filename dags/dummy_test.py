480b02f6a05b3ff8fa34979952ddf22dc43bdba2
mikaela.pisani <mikaela.pisani@rootstrap.com>
2020-12-12 15:08:13 -0300

    add env variable

--- dags/dummy_test.py
+++ dags/dummy_test.py
@@ -9,7 +9,7 @@ from datetime import datetime, timedelta
 from airflow.models import Variable
 
 import sys, os
-sys.path.insert(0,os.environ('AIRFLOW__CORE__DAGS_FOLDER'))
+sys.path.insert(0,os.environ.get('AIRFLOW__CORE__DAGS_FOLDER'))
 
 #from importlib import import_module
 #defaults = import_module('general.defaults')
