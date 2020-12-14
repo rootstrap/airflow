8e7a1b99f51b3e8d4c3dc2c0af370f81b37c0250
mikaela.pisani <mikaela.pisani@rootstrap.com>
2020-12-14 10:06:13 -0300

    add dags_utils/

--- dags/redshift_transformer.py
+++ dags/redshift_transformer.py
@@ -7,7 +7,7 @@ from airflow.models import Variable
 
 import sys, os
 AIRFLOW__CORE__DAGS_FOLDER = Variable.get("AIRFLOW__CORE__DAGS_FOLDER")
-sys.path.append('/opt/airflow/dags/repo/dags/')
+sys.path.insert(0, '/opt/airflow/dags/repo/dags/')
 
 from dags_utils.general import groups, defaults
 from dags_utils.s3 import helper as s3_helper
